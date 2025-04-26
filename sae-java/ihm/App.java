d745c09cd43228ee745484f50ae86e8f8494129e
mag <m4g0o0@proton.me>
2025-04-26 20:54:54 +0200

    Finission implémentation pour IHM

--- sae-java/ihm/App.java
+++ sae-java/ihm/App.java
@@ -13,39 +13,40 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.PasswordField;
 
 public class App extends Application {
    private TextField id;
-   private TextField mdp;
+   private PasswordField mdp;
 
    public App() {
    }
 
    public void init() {
       this.id = new TextField("");
-      this.mdp = new TextField("");
+      this.mdp = new PasswordField();
 
    }
 
    private GridPane gridPane() {
       GridPane var1 = new GridPane();
-      var1.add(new Label("Nombre 1"), 0, 0);
-      var1.add(new Label("Nombre 2"), 0, 1);
+      var1.add(new Label("Identifiant"), 0, 0);
+      var1.add(new Label("Mot de passe"), 0, 1);
       var1.add(this.id, 1, 0);
       var1.add(this.mdp, 1, 1);
-      var1.setHgap(50.0);
-      var1.setVgap(20.0);
+      var1.setHgap(80.0);
+      var1.setVgap(60.0);
       return var1;
    }
    
 
    private HBox hbox() {
-      HBox var1 = new HBox(50.0);
+      HBox var1 = new HBox(150.0);
       Button var2 = new Button("Entrer");
       Button var8 = new Button("Quitter");
       var1.getChildren().addAll(new Node[]{var2, var8});
       var1.setAlignment(Pos.CENTER);
-    //   var2.setOnAction(new ControleurEntrer(this));
+      //var2.setOnAction(new ControleurEntrer(this));
       var8.setOnAction(new ControleurQuitter(this));
       return var1;
    }
@@ -54,8 +55,8 @@ public class App extends Application {
       HBox var1 = new HBox(10.0);
       VBox var2 = new VBox(50.0);
       var2.getChildren().addAll(new Node[]{this.gridPane(), this.hbox()});
-      var2.setPrefWidth(400.0);
-      HBox.setMargin(var2, new Insets(30.0));
+      var2.setPrefWidth(500.0);
+      HBox.setMargin(var2, new Insets(200.0));
       var1.getChildren().add(var2);
       return var1;
    }
